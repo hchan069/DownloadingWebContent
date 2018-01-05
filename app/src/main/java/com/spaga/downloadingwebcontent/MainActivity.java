b1065c9d33c5808d13d51f58f03884481fb1a913
spagabtti <spagabtti@gmail.com>
2018-01-05 10:28:00 -0800

    edit

---
+++ app/src/main/java/com/spaga/downloadingwebcontent/MainActivity.java
@@ -0,0 +1,25 @@
+package com.spaga.downloadingwebcontent;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DownloadTask task = new DownloadTask();
+        task.execute("https://www.reddit.com/");
+    }
+}
