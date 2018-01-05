eb31c87646d6efecd8d22d679ccba0e54578a627
spagabtti <spagabtti@gmail.com>
2018-01-05 10:45:23 -0800

    edit

--- app/src/main/java/com/spaga/downloadingwebcontent/MainActivity.java
+++ app/src/main/java/com/spaga/downloadingwebcontent/MainActivity.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,9 +16,31 @@ public class MainActivity extends AppCompatActivity {
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
         @Override
-        protected String doInBackground(String... strings) {
-            Log.i("URL", strings[0]);
-            return "Done";
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection)url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char)data;
+                    result += current;
+                    data = reader.read();
+                }
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return "Failed!";
+            }
+
         }
     }
 
@@ -32,6 +58,6 @@ public class MainActivity extends AppCompatActivity {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
-        Log.i("Result", result);
+        Log.i("Content of URL", result);
     }
 }
